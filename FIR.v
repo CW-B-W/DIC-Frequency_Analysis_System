8c209a89467eab44694e126ac8f8d005c175318f
CW-B-W <cw.brad.wang@gmail.com>
2021-06-28 22:51:01 +0800

    Use `wire` to implement FIR_C[]

--- FIR.v
+++ FIR.v
@@ -11,21 +11,42 @@ reg         [15:0] sig      [31:0]; /* should always be positive */
 wire        [23:0] v        [31:0]; /* 1 bit, 7 bits, 16 bits    */
 wire        [23:0] y;
 
-reg         [19:0] FIR_C [31:0];
+wire        [19:0] FIR_C [31:0];
 
 integer i;
 
-task fp_mul;
-    input  signed     [19:0] vc; /* 1 bit, 3 bits, 16 bits */
-    input  signed     [15:0] vx; /* 1 bit, 7 bits,  8 bits */
-    output reg signed [23:0] vy; /* 1 bit, 7 bits, 16 bits */
-    reg signed        [31:0] vt; /* intermediate value     */
-
-    begin
-        vt = vc * vx;
-        vy = vt[31:8];
-    end
-endtask
+assign FIR_C[00] = 20'hFFF9E;
+assign FIR_C[01] = 20'hFFF86;
+assign FIR_C[02] = 20'hFFFA7;
+assign FIR_C[03] = 20'h0003B;
+assign FIR_C[04] = 20'h0014B;
+assign FIR_C[05] = 20'h0024A;
+assign FIR_C[06] = 20'h00222;
+assign FIR_C[07] = 20'hFFFE4;
+assign FIR_C[08] = 20'hFFBC5;
+assign FIR_C[09] = 20'hFF7CA;
+assign FIR_C[10] = 20'hFF74E;
+assign FIR_C[11] = 20'hFFD74;
+assign FIR_C[12] = 20'h00B1A;
+assign FIR_C[13] = 20'h01DAC;
+assign FIR_C[14] = 20'h02F9E;
+assign FIR_C[15] = 20'h03AA9;
+assign FIR_C[16] = 20'h03AA9;
+assign FIR_C[17] = 20'h02F9E;
+assign FIR_C[18] = 20'h01DAC;
+assign FIR_C[19] = 20'h00B1A;
+assign FIR_C[20] = 20'hFFD74;
+assign FIR_C[21] = 20'hFF74E;
+assign FIR_C[22] = 20'hFF7CA;
+assign FIR_C[23] = 20'hFFBC5;
+assign FIR_C[24] = 20'hFFFE4;
+assign FIR_C[25] = 20'h00222;
+assign FIR_C[26] = 20'h0024A;
+assign FIR_C[27] = 20'h0014B;
+assign FIR_C[28] = 20'h0003B;
+assign FIR_C[29] = 20'hFFFA7;
+assign FIR_C[30] = 20'hFFF86;
+assign FIR_C[31] = 20'hFFF9E;
 
 always@(posedge clk, posedge rst) begin
     if (rst) begin
@@ -35,39 +56,6 @@ always@(posedge clk, posedge rst) begin
         for (i = 0; i < 32; i = i + 1) begin
             sig[i] <= 0;
         end
-
-        FIR_C[00] = 20'hFFF9E ;
-        FIR_C[01] = 20'hFFF86 ;
-        FIR_C[02] = 20'hFFFA7 ;
-        FIR_C[03] = 20'h0003B ;
-        FIR_C[04] = 20'h0014B ;
-        FIR_C[05] = 20'h0024A ;
-        FIR_C[06] = 20'h00222 ;
-        FIR_C[07] = 20'hFFFE4 ;
-        FIR_C[08] = 20'hFFBC5 ;
-        FIR_C[09] = 20'hFF7CA ;
-        FIR_C[10] = 20'hFF74E ;
-        FIR_C[11] = 20'hFFD74 ;
-        FIR_C[12] = 20'h00B1A ;
-        FIR_C[13] = 20'h01DAC ;
-        FIR_C[14] = 20'h02F9E ;
-        FIR_C[15] = 20'h03AA9 ;
-        FIR_C[16] = 20'h03AA9 ;
-        FIR_C[17] = 20'h02F9E ;
-        FIR_C[18] = 20'h01DAC ;
-        FIR_C[19] = 20'h00B1A ;
-        FIR_C[20] = 20'hFFD74 ;
-        FIR_C[21] = 20'hFF74E ;
-        FIR_C[22] = 20'hFF7CA ;
-        FIR_C[23] = 20'hFFBC5 ;
-        FIR_C[24] = 20'hFFFE4 ;
-        FIR_C[25] = 20'h00222 ;
-        FIR_C[26] = 20'h0024A ;
-        FIR_C[27] = 20'h0014B ;
-        FIR_C[28] = 20'h0003B ;
-        FIR_C[29] = 20'hFFFA7 ;
-        FIR_C[30] = 20'hFFF86 ;
-        FIR_C[31] = 20'hFFF9E ;
     end
     else begin
         if (sig_idx >= 32) begin
